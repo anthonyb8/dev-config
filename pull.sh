ae622872851ac3f5336ecb73bba3f527eb379392
anthonyb8 <anthonybaxter819@gmail.com>
2025-07-13 09:47:13 -0400

    Update configuration files

--- pull.sh
+++ pull.sh
@@ -1,65 +1,103 @@
 #!/bin/bash
 
-# Define the GitHub repo directory
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+os() {
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    if [[ -f /etc/os-release ]]; then
+      . /etc/os-release
+      echo "$ID"
+    else
+      echo "unknown"
+    fi
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "darwin"
+  fi
+}
 
 # Define the directories/files to restore
 CONFIG_FILES=(
-	"$SCRIPT_DIR/nvim"          # Example: Neovim config
-	"$SCRIPT_DIR/kitty"         # Example: Kitty terminal config
-	"$SCRIPT_DIR/.tmux.conf"    # Example: Tmux config
-	"$SCRIPT_DIR/starship.toml" # Starship config
+  "$SCRIPT_DIR/nvim"
+  "$SCRIPT_DIR/.tmux.conf"
 )
 
 # Define where to move the configurations on the machine
 TARGET_DIRS=(
-	"$HOME/.config/nvim"
-	"$HOME/.config/kitty"
-	"$HOME/.tmux.conf"
-	"$HOME/.config/starship.toml"
+  "$HOME/.config/nvim"
+  "$HOME/.tmux.conf"
 )
+# Define the GitHub repo directory
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+OS=$(os)
 
-# Pull the latest changes from GitHub
-cd "$SCRIPT_DIR" || exit
-# git pull
+git pull
+echo "$OS"
 
 # Restore the configuration files by movi# Restore the configuration files with backups
 for i in "${!CONFIG_FILES[@]}"; do
-	# Check if the target exists
-	if [[ -e "${TARGET_DIRS[i]}" ]]; then
-		# If a backup already exists, remove it to avoid errors
-		if [[ -e "${TARGET_DIRS[i]}_bak" ]]; then
-			rm -rf "${TARGET_DIRS[i]}_bak"
-		fi
-		# Rename the existing target to a backup
-		mv "${TARGET_DIRS[i]}" "${TARGET_DIRS[i]}_bak"
-		echo "Backup created: ${TARGET_DIRS[i]} -> ${TARGET_DIRS[i]}_bak"
-	fi
+  # Check if the target exists
+  if [[ -e "${TARGET_DIRS[i]}" ]]; then
+    # If a backup already exists, remove it to avoid errors
+    if [[ -e "${TARGET_DIRS[i]}_bak" ]]; then
+      rm -rf "${TARGET_DIRS[i]}_bak"
+    fi
+    # Rename the existing target to a backup
+    mv "${TARGET_DIRS[i]}" "${TARGET_DIRS[i]}_bak"
+    echo "Backup created: ${TARGET_DIRS[i]} -> ${TARGET_DIRS[i]}_bak"
+  fi
 
-	# Copy the new configuration to the target
-	if [[ -d "${CONFIG_FILES[i]}" ]]; then
-		# If source is a directory, copy the directory
-		cp -r "${CONFIG_FILES[i]}" "${TARGET_DIRS[i]}"
-	elif [[ -f "${CONFIG_FILES[i]}" ]]; then
-		# If source is a file, copy the file
-		cp "${CONFIG_FILES[i]}" "${TARGET_DIRS[i]}"
-	fi
+  # Copy the new configuration to the target
+  if [[ -d "${CONFIG_FILES[i]}" ]]; then
+    # If source is a directory, copy the directory
+    cp -r "${CONFIG_FILES[i]}" "${TARGET_DIRS[i]}"
+  elif [[ -f "${CONFIG_FILES[i]}" ]]; then
+    # If source is a file, copy the file
+    cp "${CONFIG_FILES[i]}" "${TARGET_DIRS[i]}"
+  fi
 done
 
 # .zshrc
 mv "$HOME/.zshrc" "$HOME/.zshrc_bak"
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	cp "$SCRIPT_DIR/zsh/linux/.zshrc" "$HOME/.zshrc"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-	cp "$SCRIPT_DIR/zsh/macos/.zshrc" "$HOME/.zshrc"
-fi
+case "$OS" in
+"arch")
+  cp "$SCRIPT_DIR/zsh/arch/.zshrc" "$HOME/.zshrc"
+  ;;
+"debian")
+  cp "$SCRIPT_DIR/zsh/debian/.zshrc" "$HOME/.zshrc"
+  ;;
+"darwin")
+  cp "$SCRIPT_DIR/zsh/macos/.zshrc" "$HOME/.zshrc"
+  ;;
+*)
+  echo "Invalid OS."
+  ;;
+esac
+
+# if [[ "$OS" == "linux-gnu"* ]]; then
+#   cp "$SCRIPT_DIR/zsh/linux/.zshrc" "$HOME/.zshrc"
+# elif [[ "$OSTYPE" == "darwin"* ]]; then
+#   cp "$SCRIPT_DIR/zsh/macos/.zshrc" "$HOME/.zshrc"
+# fi
 
 # alacritty.toml
 mv "$HOME/.config/alacritty/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml_bak"
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	cp "$SCRIPT_DIR/alacritty/linux/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-	cp "$SCRIPT_DIR/alacritty/macos/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml"
-fi
+case "$OS" in
+"arch")
+  cp "$SCRIPT_DIR/alacritty/arch/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml"
+  ;;
+"debian")
+  cp "$SCRIPT_DIR/alacritty/debian/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml"
+  ;;
+"darwin")
+  cp "$SCRIPT_DIR/alacritty/macos/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml"
+  ;;
+*)
+  echo "Invalid OS."
+  ;;
+esac
+
+# if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+#   cp "$SCRIPT_DIR/alacritty/linux/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml"
+# elif [[ "$OSTYPE" == "darwin"* ]]; then
+#   cp "$SCRIPT_DIR/alacritty/macos/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml"
+# fi
--- push.sh
+++ push.sh
@@ -1,32 +1,64 @@
 #!/bin/bash
 
+os() {
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    if [[ -f /etc/os-release ]]; then
+      . /etc/os-release
+      echo "$ID"
+    else
+      echo "unknown"
+    fi
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "darwin"
+  fi
+}
+
 # Define the directories/files to back up
 CONFIG_FILES=(
-	"$HOME/.config/nvim"          # Neovim config
-	"$HOME/.config/kitty"         # Kitty terminal config
-	"$HOME/.tmux.conf"            # Tmux config
-	"$HOME/.config/starship.toml" # Starship config
+  # "$HOME/.config/nvim" # Neovim config
+  # "$HOME/.tmux.conf" # Tmux config
+  # "$HOME/.config/starship.toml" # Starship config
+  # "$HOME/.config/kitty"         # Kitty terminal config
 )
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+OS=$(os)
 
 # Copy the configuration files to the GitHub repository directory
 for file in "${CONFIG_FILES[@]}"; do
-	cp -r "$file" "$SCRIPT_DIR/"
+  cp -r "$file" "$SCRIPT_DIR/"
 done
 
 # cp -r "$HOME/.config/alacritty/alacritty.toml" "$SCRIPT_DIR/alacritty/"
 
 # .zshrc
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-	cp "$HOME/.zshrc" "$SCRIPT_DIR/zsh/linux/"
-	cp "$HOME/.config/alacritty/alacritty.toml" "$SCRIPT_DIR/alacritty/linux/"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-	cp "$HOME/.zshrc" "$SCRIPT_DIR/zsh/macos/"
-	cp "$HOME/.config/alacritty/alacritty.toml" "$SCRIPT_DIR/alacritty/macos/"
-fi
+# if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+#   cp "$HOME/.zshrc" "$SCRIPT_DIR/zsh/linux/"
+#   cp "$HOME/.config/alacritty/alacritty.toml" "$SCRIPT_DIR/alacritty/linux/"
+# elif [[ "$OSTYPE" == "darwin"* ]]; then
+#   cp "$HOME/.zshrc" "$SCRIPT_DIR/zsh/macos/"
+#   cp "$HOME/.config/alacritty/alacritty.toml" "$SCRIPT_DIR/alacritty/macos/"
+# fi
+
+case "$OS" in
+"arch")
+  cp "$HOME/.zshrc" "$SCRIPT_DIR/zsh/arch/"
+  cp "$HOME/.config/alacritty/alacritty.toml" "$SCRIPT_DIR/alacritty/arch/"
+  ;;
+"debian")
+  cp "$HOME/.zshrc" "$SCRIPT_DIR/zsh/linux/"
+  cp "$HOME/.config/alacritty/alacritty.toml" "$SCRIPT_DIR/alacritty/debian/"
+  ;;
+"darwin")
+  cp "$HOME/.zshrc" "$SCRIPT_DIR/zsh/macos/"
+  cp "$HOME/.config/alacritty/alacritty.toml" "$SCRIPT_DIR/alacritty/macos/"
+  ;;
+*)
+  echo "Invalid OS."
+  ;;
+esac
 
 # Add, commit, and push the changes to GitHub
-# git add .
-# git commit -m "Update configuration files"
-# git push origin main
+git add .
+git commit -m "Update configuration files"
+git push origin main
