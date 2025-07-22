af05ebc2a83a8e0bb086d4d6995e39d61aa6f828
anthonyb8 <anthonybaxter819@gmail.com>
2025-07-22 07:43:03 -0400

    Update configuration files

--- pull.sh
+++ pull.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+AUTO_YES=false
+
+# Parse the -y flag
+if [ "$1" == "-y" ]; then
+  AUTO_YES=true
+fi
+
 os() {
   if [[ "$OSTYPE" == "linux-gnu"* ]]; then
     if [[ -f /etc/os-release ]]; then
@@ -29,66 +36,81 @@ TARGET_DIRS=(
   "$HOME/.tmux.conf"
 )
 
-#git pull
-echo "$OS"
+git pull
 
-# Restore the configuration files by movi# Restore the configuration files with backups
-for i in "${!CONFIG_FILES[@]}"; do
-  # Check if the target exists
-  if [[ -e "${TARGET_DIRS[i]}" ]]; then
-    # If a backup already exists, remove it to avoid errors
-    if [[ -e "${TARGET_DIRS[i]}_bak" ]]; then
-      rm -rf "${TARGET_DIRS[i]}_bak"
+update() {
+  # Restore the configuration files by movi# Restore the configuration files with backups
+  for i in "${!CONFIG_FILES[@]}"; do
+    # Check if the target exists
+    if [[ -e "${TARGET_DIRS[i]}" ]]; then
+      # If a backup already exists, remove it to avoid errors
+      if [[ -e "${TARGET_DIRS[i]}_bak" ]]; then
+        rm -rf "${TARGET_DIRS[i]}_bak"
+      fi
+      # Rename the existing target to a backup
+      mv "${TARGET_DIRS[i]}" "${TARGET_DIRS[i]}_bak"
+      echo "Backup created: ${TARGET_DIRS[i]} -> ${TARGET_DIRS[i]}_bak"
     fi
-    # Rename the existing target to a backup
-    mv "${TARGET_DIRS[i]}" "${TARGET_DIRS[i]}_bak"
-    echo "Backup created: ${TARGET_DIRS[i]} -> ${TARGET_DIRS[i]}_bak"
+
+    # Copy the new configuration to the target
+    if [[ -d "${CONFIG_FILES[i]}" ]]; then
+      # If source is a directory, copy the directory
+      cp -r "${CONFIG_FILES[i]}" "${TARGET_DIRS[i]}"
+      echo "Copied ${CONFIG_FILES[i]} ${TARGET_DIRS[i]}"
+    elif [[ -f "${CONFIG_FILES[i]}" ]]; then
+      # If source is a file, copy the file
+      cp "${CONFIG_FILES[i]}" "${TARGET_DIRS[i]}"
+      echo "Copied ${CONFIG_FILES[i]} ${TARGET_DIRS[i]}"
+    fi
+  done
+
+  # .zshrc
+  mv "$HOME/.zshrc" "$HOME/.zshrc_bak"
+
+  case "$OS" in
+  "arch")
+    cp "$SCRIPT_DIR/zsh/arch/.zshrc" "$HOME/.zshrc"
+    ;;
+  "debian")
+    cp "$SCRIPT_DIR/zsh/debian/.zshrc" "$HOME/.zshrc"
+    ;;
+  "darwin")
+    cp "$SCRIPT_DIR/zsh/macos/.zshrc" "$HOME/.zshrc"
+    ;;
+  *)
+    echo "Invalid OS."
+    ;;
+  esac
+
+  # alacritty.toml
+  mv "$HOME/.config/alacritty/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml_bak"
+
+  case "$OS" in
+  "arch")
+    cp "$SCRIPT_DIR/alacritty/arch/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml"
+    ;;
+  "debian")
+    cp "$SCRIPT_DIR/alacritty/debian/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml"
+    ;;
+  "darwin")
+    cp "$SCRIPT_DIR/alacritty/macos/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml"
+    ;;
+  *)
+    echo "Invalid OS."
+    ;;
+  esac
+}
+
+# Add, commit, and push the changes to GitHub
+if [ "$AUTO_YES" == true ]; then
+  update
+else
+  read -rp "Do you want to update system config? [y/N]" answer
+  if [[ "$answer" != "y" && "$answer" != "Y" ]]; then
+    echo "Skipping system update"
+    exit 0
+  else
+    update
   fi
 
-  # Copy the new configuration to the target
-  if [[ -d "${CONFIG_FILES[i]}" ]]; then
-    # If source is a directory, copy the directory
-    cp -r "${CONFIG_FILES[i]}" "${TARGET_DIRS[i]}"
-    echo "Copied ${CONFIG_FILES[i]} ${TARGET_DIRS[i]}"
-  elif [[ -f "${CONFIG_FILES[i]}" ]]; then
-    # If source is a file, copy the file
-    cp "${CONFIG_FILES[i]}" "${TARGET_DIRS[i]}"
-    echo "Copied ${CONFIG_FILES[i]} ${TARGET_DIRS[i]}"
-  fi
-done
-
-# .zshrc
-mv "$HOME/.zshrc" "$HOME/.zshrc_bak"
-
-case "$OS" in
-"arch")
-  cp "$SCRIPT_DIR/zsh/arch/.zshrc" "$HOME/.zshrc"
-  ;;
-"debian")
-  cp "$SCRIPT_DIR/zsh/debian/.zshrc" "$HOME/.zshrc"
-  ;;
-"darwin")
-  cp "$SCRIPT_DIR/zsh/macos/.zshrc" "$HOME/.zshrc"
-  ;;
-*)
-  echo "Invalid OS."
-  ;;
-esac
-
-# alacritty.toml
-mv "$HOME/.config/alacritty/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml_bak"
-
-case "$OS" in
-"arch")
-  cp "$SCRIPT_DIR/alacritty/arch/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml"
-  ;;
-"debian")
-  cp "$SCRIPT_DIR/alacritty/debian/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml"
-  ;;
-"darwin")
-  cp "$SCRIPT_DIR/alacritty/macos/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml"
-  ;;
-*)
-  echo "Invalid OS."
-  ;;
-esac
+fi
