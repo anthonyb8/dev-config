5d39a033dd2dc395926c999d1dc5ed1974fbbaea
anthonyb8 <anthonybaxter819@gmail.com>
2026-05-20 11:41:41 -0400

    Updated startup.sh

--- workspace/startup.sh
+++ workspace/startup.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
+sleep 5
+
 chromium &
-sleep 1
+sleep 3
 wmctrl -r "Chromium" -t 0
 wmctrl -r "Chromium" -b add,maximized_vert,maximized_horz
 
 alacritty &
-sleep 1
+sleep 3
 wmctrl -r "Alacritty" -t 1
 wmctrl -r "Alacritty" -b add,maximized_vert,maximized_horz
 
 thunar &
-sleep 1
+sleep 3
 wmctrl -r "Thunar" -t 2
 wmctrl -r "Thunar" -b add,maximized_vert,maximized_horz
 
 alacritty --title "rmpc" -e rmpc &
-sleep 1
+sleep 3
 wmctrl -r "rmpc" -t 3
 wmctrl -r "rmpc" -b add,maximized_vert,maximized_horz
